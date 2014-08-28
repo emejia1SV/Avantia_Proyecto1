b58a28670412efd768439c388ca050c95975d965
emejia1SV <edwinmejia_1@hotmail.com>
2014-08-28 09:28:15 -0600

    comentarios
    Se comento toda la clase para su futura implementación

--- DepuradorAgregadores/src/sv/avantia/depurador/agregadores/jdbc/SessionFactoryUtil.java
+++ DepuradorAgregadores/src/sv/avantia/depurador/agregadores/jdbc/SessionFactoryUtil.java
@@ -1,16 +1,14 @@
 package sv.avantia.depurador.agregadores.jdbc;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
+
 /** * @author hennebrueder This class garanties that only one single SessionFactory 
  * *         is instanciated and that the configuration is done thread safe as 
  * *         singleton. Actually it only wraps the Hibernate SessionFactory. 
  * *         You are free to use any kind of JTA or Thread transactionFactories. */
 public class SessionFactoryUtil {  
-	/** The single instance of hibernate SessionFactory */  
+	/** The single instance of hibernate SessionFactory *//*  
 	private static org.hibernate.SessionFactory sessionFactory;	
-	/**	 * disable contructor to guaranty a single instance	 */	
+	*//**	 * disable contructor to guaranty a single instance	 *//*	
 	private SessionFactoryUtil() {	}	
 	static{
 		// Annotation and XML    
@@ -19,17 +17,17 @@ public class SessionFactoryUtil {
 		sessionFactory = new Configuration().configure().buildSessionFactory();  }	
 	public static SessionFactory getInstance() {		
 		return sessionFactory;	}  
-	/**   * Opens a session and will not bind it to a session context   * @return the session   */	
+	*//**   * Opens a session and will not bind it to a session context   * @return the session   *//*	
 	public Session openSession() {		return sessionFactory.openSession();	}	
-	/**   Returns a session from the session context. If there is no session in the context it opens a session,   * stores it in the context and returns it.	 
+	*//**   Returns a session from the session context. If there is no session in the context it opens a session,   * stores it in the context and returns it.	 
 	 * This factory is intended to be used with a hibernate.cfg.xml	 
 	 *  including the following property &lt;property	 
-	 *  name="current_session_context_class"&gt;thread&lt;/property&gt; This would return	 * the current open session or if this does not exist, will create a new	 * session	 * 	 * @return the session	 */	
+	 *  name="current_session_context_class"&gt;thread&lt;/property&gt; This would return	 * the current open session or if this does not exist, will create a new	 * session	 * 	 * @return the session	 *//*	
 	public Session getCurrentSession() {		
 		return sessionFactory.getCurrentSession();	}  
-	/**   * closes the session factory   */	
+	*//**   * closes the session factory   *//*	
 	public static void close(){		
 		if (sessionFactory != null)			sessionFactory.close();		sessionFactory = null;	}}
-	}
+	}*/
 }
  
